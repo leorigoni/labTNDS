881ab39efe77c4df01bab1cc3fcb163c0eeb760f
leorigoni <leonardo.rigoni.04@gmail.com>
2024-12-25 20:01:47 +0100

    new file:   esercizilezioni/consegnezip/esercizio10.1.tar.gz 	new file:   esercizilezioni/lezione10/esercizio10.1/esercizio10.1 	modified:   esercizilezioni/lezione10/esercizio10.1/esercizio10.1.cc 	new file:   esercizilezioni/lezione10/esercizio10.1/random.pdf 	modified:   esercizilezioni/lezione10/esercizio10.1/randomgen.h 	new file:   esercizilezioni/lezione10/esercizio10.1/varianza.pdf

--- esercizilezioni/lezione10/esercizio10.1/esercizio10.1.cc
+++ esercizilezioni/lezione10/esercizio10.1/esercizio10.1.cc
@@ -2,39 +2,63 @@
 #include "TCanvas.h"
 #include "TH1F.h"
 #include "TAxis.h"
+#include "TGraph.h"
 #include "randomgen.h" 
 
 int main(){
     Randomgen r(1);
-    int nmax=100000;
-    TH1F u("uniforme", "uniforme", 70, 3, 12);
-    TH1F e("esponenziale", "esponenziale", 70, 0, 10);
-    TH1F gbm("gaussiana b-m", "gaussiana b-m", 70, -4, 6);
-    TH1F gar("gaussiana a-r", "gaussiana a-r", 70, -5, 7);
-    for(int k=0; k<nmax; k++){
-        u.Fill(r.unif(5, 10));
-        e.Fill(r.exp(1));
-        gbm.Fill(r.gausbm(1, 1));
-        gar.Fill(r.gausar(1, 1, -4, 6));
+    int nmax=1000000;
+    vector<TH1F*> u;
+    TGraph var;
+    for(int i=0; i<12; i++){
+        TString name=Form("legenda %d", i+1);
+        TString title=Form("Uniforme %d", i+1);
+        u.push_back(new TH1F(name, title, 70, 0, i+1));
+    }
+    for(int i=0; i<12; i++){
+        u[i]->SetMinimum(0);
+        //double totsum=0;
+        //double totsumsq=0;
+        //int tot=0;
+        for(int k=0; k<nmax; k+=i+1){
+            double sum=0;
+            for(int j=1; j<=i+1; j++){
+                sum+=r.unif(0, 1);
+            }
+            //tot++;
+            //totsum+=sum;
+            //double mean=totsum/nmax;
+            //totsumsq+=pow(sum-mean, 2);
+            u[i]->Fill(sum);
+        }
+        //var.SetPoint(i, i+1, totsumsq/(tot-1));
     }
     TCanvas c("randomgen", "randomgen");
     c.cd();
-    c.Divide(2, 2);
-    c.cd(1);
-    u.GetXaxis()->SetTitle("x [AU]");
-    u.GetYaxis()->SetTitle("N");
-    u.Draw();
-    c.cd(2);
-    e.GetXaxis()->SetTitle("x [AU]");
-    e.GetYaxis()->SetTitle("N");
-    e.Draw();
-    c.cd(3);
-    gbm.GetXaxis()->SetTitle("x [AU]");
-    gbm.GetYaxis()->SetTitle("N");
-    gbm.Draw();
-    c.cd(4);
-    gar.GetXaxis()->SetTitle("x [AU]");
-    gar.GetYaxis()->SetTitle("N");
-    gar.Draw();
+    c.Divide(3, 4);
+    for(int i=0; i<12; i++){
+        c.cd(i+1);
+        u[i]->GetXaxis()->SetTitle("x [AU]");
+        u[i]->GetYaxis()->SetTitle("N");
+        u[i]->Draw();                
+    }
     c.SaveAs("random.pdf");
+    for(int i=0; i<12; i++){
+        double stddev=u[i]->GetStdDev();
+        double varianza=pow(stddev, 2);
+        var.SetPoint(i, i+1, varianza);
+    }
+    TCanvas c1("c_varianza", "Grafico Varianza", 800, 600);
+    c1.SetGrid();
+    var.SetTitle("Varianza per ogni istogramma");
+    var.GetXaxis()->SetTitle("Indice Istogramma");
+    var.GetYaxis()->SetTitle("Varianza");
+    var.SetMarkerColor(kBlack);
+    var.SetMarkerSize(1.2);
+    var.SetMarkerStyle(20);
+    var.SetLineColor(kRed);
+    var.SetLineWidth(1);
+    var.Draw("APL");
+    c1.SaveAs("varianza.pdf");
+    return 0;
 }
